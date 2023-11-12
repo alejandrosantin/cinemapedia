ada48c9f10979f9b5e8ca29dedd71076a0e5aac4
Alejandro <alekungfumdp@gmail.com>
2023-11-12 20:05:02 -0300

    environments config

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -7,7 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: Center(
+        child: Text('cinemapedia'),
+      ),
     );
   }
 }
